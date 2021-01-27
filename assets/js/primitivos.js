e755517a26833ae5c9863c9135f056c605256460
Stalin Caiche <scaiche@netlife.net.ec>
2021-01-26 23:26:38 -0500

    0001 - Datos Primitivos

---
+++ assets/js/primitivos.js
@@ -0,0 +1,32 @@
+let nombre  = 'Peter Parker';
+console.log(nombre);
+
+nombre = 'Ben Parker';
+console.log(nombre);
+
+nombre = `Tía May`;
+console.log( typeof nombre );
+
+nombre = 123;
+console.log( typeof nombre );
+
+
+let esMarvel = false;
+console.log( typeof esMarvel );
+
+let edad = 33;
+console.log( typeof edad );
+
+edad = 33.1;
+console.log( typeof edad );
+
+//camelCase
+let superPoderDeSpiderman;
+console.log( typeof superPoderDeSpiderman );
+
+let soyNull = null;
+console.log( typeof soyNull );
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log( typeof symbol1 );
